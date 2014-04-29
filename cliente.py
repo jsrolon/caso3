b547c190ef39084172e15bdd45bf4baa8471a99a
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2014-04-28 23:03:19 -0500

    cambios al script cliente y a los properties

--- cliente.py
+++ cliente.py
@@ -1,5 +1,6 @@
 import fileinput
 import subprocess
+import re
 
 tipo = './InfracompNoSeguridad/'
 properties = 'cliente.properties'
@@ -7,32 +8,23 @@ jar = 'cliente.jar'
 
 for repeticiones in (0,6):
 	for line in fileinput.input(tipo + properties, inplace=True):
-		if line.find('gap=')!=-1:
-			print('gap=',20000,end='')
-		elif line.find('number=')!=-1:
-			print('number=',400, end='')
-		else:
-			print('times=',repeticiones, end='')
+		print(re.sub('(gap=)[0-9]+$', '\1{}'.format(20), line), end='')
+		print(re.sub('(number=)[0-9]+$', '\1{}'.format(400), line), end='')
+		print(re.sub('(times=)[0-9]+$', '\1{}'.format(repeticiones), line), end='')
 	# Ejecutar JAR
 	subprocess.call(['java', '-jar', tipo + jar])
 
 	for line in fileinput.input(tipo + properties, inplace=True):
-		if line.find('gap=')!=-1:
-			print('gap=',40000,end='')
-		elif line.find('number=')!=-1:
-			print('number=',200, end='')
-		else:
-			print('times=',repeticiones, end='')
+		print(re.sub('(gap=)[0-9]+$', '\1{}'.format(40), line), end='')
+		print(re.sub('(number=)[0-9]+$', '\1{}'.format(200), line), end='')
+		print(re.sub('(times=)[0-9]+$', '\1{}'.format(repeticiones), line), end='')
 	# Ejecutar JAR
 	subprocess.call(['java', '-jar', tipo + jar])
 
 	for line in fileinput.input(tipo + properties, inplace=True):
-		if line.find('gap=')!=-1:
-			print('gap=',80000,end='')
-		elif line.find('number=')!=-1:
-			print('number=',100, end='')
-		else:
-			print('times=',repeticiones, end='')
+		print(re.sub('(gap=)[0-9]+$', '\1{}'.format(80), line), end='')
+		print(re.sub('(number=)[0-9]+$', '\1{}'.format(100), line), end='')
+		print(re.sub('(times=)[0-9]+$', '\1{}'.format(repeticiones), line), end='')
 	# Ejecutar JAR
 	subprocess.call(['java', '-jar', tipo + jar])
 	
